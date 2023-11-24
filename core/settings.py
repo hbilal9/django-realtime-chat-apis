9c4891b3f30ae87188b8b760f93ee0d41e89c20a
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2023-11-24 11:01:30 +0500

    Cors header setup

--- core/settings.py
+++ core/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
 
     # Third Party Apps
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,6 +124,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+# CORS_URLS_REGEX = r"^/(api|media)/.*$"
+# CORS_ALLOWED_ORIGINS = [
+#     "https://127.0.0.1:3000",
+#     "http://localhost:3000",
+#     'https://skyive.com',
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
