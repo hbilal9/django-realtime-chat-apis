0e3b1f456ebc2395f8823ec1ff713aa44d3c396f
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2023-11-28 12:00:01 +0500

    authSerializer => RegisterSerializer created

--- api/serializers/authSerializer.py
+++ api/serializers/authSerializer.py
@@ -1,10 +1,62 @@
 from rest_framework import serializers
 from api.models import User
+import re
 
 class LoginSerializer(serializers.Serializer):
     # username = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField()
+
+class RegisterSerializer(serializers.Serializer):
+    def validate_username(self, value):
+        pattern = r"^[a-zA-Z][a-zA-Z0-9_.]*$"
+        if not re.match(pattern, value):
+            raise serializers.ValidationError(
+                'Invalid username. It should start with a letter and can contain letters, numbers, underscores, and dots.'
+            )
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username already exists.")
+        if value.len() < 5:
+            raise serializers.ValidationError("Username must be at least 5 characters long.")
+        return value
+
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    confirm_password = serializers.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('confirm_password')
+    
+    def validate_username(self, username):
+        try:
+            User.objects.get(username=username)
+            raise serializers.ValidationError("This username already exists.")
+        except User.DoesNotExist:
+            pass
+        pattern = r"^[a-zA-Z][a-zA-Z0-9_.]*$"
+        if not re.match(pattern, username):
+            raise serializers.ValidationError(
+                'Invalid username. It should start with a letter and can contain letters, numbers, underscores, and dots.'
+            )
+        return username
+    def validate_email(self, value):
+        try:
+            User.objects.get(email=value)
+            raise serializers.ValidationError("This email already exists.")
+        except User.DoesNotExist:
+            pass
+        return value
+    
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        if password != self.initial_data['confirm_password']:
+            raise serializers.ValidationError("Confirm password must match.")
+        return password
 
 class UserSerializer(serializers.ModelSerializer):
     account_type = serializers.CharField(read_only=True)
