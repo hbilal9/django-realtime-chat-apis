3b7f7783ecf478bf185d4786c3b70fda14dcc80c
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2023-11-28 13:17:14 +0500

    ChatSerializer => the other person in thread details fetched

---
+++ api/serializers/chatSerializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from api.models import Thread, Message
+
+class ThreadSerializer(serializers.ModelSerializer):
+    first_person = serializers.SerializerMethodField()
+    second_person = serializers.SerializerMethodField()
+    class Meta:
+        model = Thread
+        fields = ('id', 'first_person', 'second_person', 'created_at', 'updated_at')
+
+    def get_first_person(self, obj):
+        return {
+            'id': obj.first_person.id,
+            'username': obj.first_person.username,
+            'first_name': obj.first_person.first_name,
+            'last_name': obj.first_person.last_name,
+        } if obj.first_person.id != self.context['request'].user.id else obj.first_person.id
+    
+    def get_second_person(self, obj):
+        return {
+            'id': obj.second_person.id,
+            'username': obj.second_person.username,
+            'first_name': obj.second_person.first_name,
+            'last_name': obj.second_person.last_name,
+        } if obj.second_person.id != self.context['request'].user.id else obj.second_person.id
+class MessageSerializer(serializers.ModelSerializer):
+    thread = ThreadSerializer(read_only=True)
+    class Meta:
+        model = Message
+        fields = ('id', 'thread', 'sender', 'text', 'created_at', 'updated_at')
