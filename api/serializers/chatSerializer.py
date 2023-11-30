ab8ee79bd08b59b4c38f0dec33a9a4b4da57e4ea
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2023-11-30 14:26:13 +0500

    ThreadUserSerializer created

--- api/serializers/chatSerializer.py
+++ api/serializers/chatSerializer.py
@@ -31,3 +31,30 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ('id', 'thread', 'sender', 'text', 'created_at', 'updated_at')
+
+class ThreadUserSerializer(serializers.ModelSerializer):
+    first_person = serializers.SerializerMethodField()
+    second_person = serializers.SerializerMethodField()
+    class Meta:
+        model = Thread
+        fields = ('id', 'first_person', 'second_person', 'created_at', 'updated_at')
+
+    def get_first_person(self, obj):
+        return {
+            'id': obj.first_person.id,
+            'username': obj.first_person.username,
+            'avatar': obj.first_person.avatar.url if obj.first_person.avatar else '',
+            'full_name': obj.first_person.fullName(),
+            'last_seen': obj.first_person.last_login,
+            'active_status': obj.first_person.remarks,
+        }
+    
+    def get_second_person(self, obj):
+        return {
+            'id': obj.second_person.id,
+            'username': obj.second_person.username,
+            'avatar': obj.second_person.avatar.url if obj.second_person.avatar else '',
+            'full_name': obj.second_person.fullName(),
+            'last_seen': obj.first_person.last_login,
+            'active_status': obj.second_person.remarks,
+        }
